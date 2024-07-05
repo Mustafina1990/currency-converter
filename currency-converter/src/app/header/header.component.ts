cabd8963fa4ca48c1582efc16b857cbbf26423ca
Yuliia Mustafina <71671138+Mustafina1990@users.noreply.github.com>
2024-07-05 21:03:59 +0200

    chore: Update header component with currency exchange rates

--- currency-converter/src/app/header/header.component.ts
+++ currency-converter/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.sass']
 })
 export class HeaderComponent {
+  exchangeRates: { [key: string]: number } = {};
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetchExchangeRates();
+  }
+
+  fetchExchangeRates(): void {
+    const apiKey = 'fca_live_Hanq18H87GsTqtthJOY0tIS8Y2AWTaxe3oXQXTiS';
+    const apiUrl = `https://api.freecurrencyapi.com/v1/latest?base_currency=USD&apikey=${apiKey}`;
+
+    this.http.get(apiUrl).subscribe((data: any) => {
+      this.exchangeRates = data.rates;
+    });
+  }
 }
