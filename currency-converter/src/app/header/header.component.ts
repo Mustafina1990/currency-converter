fb78294c8f9d4983a6a9b8305c4231160e236036
Yuliia Mustafina <71671138+Mustafina1990@users.noreply.github.com>
2024-09-18 20:23:07 +0200

    Refactor HeaderComponent to fetch and display currency rates

--- currency-converter/src/app/header/header.component.ts
+++ currency-converter/src/app/header/header.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { CurrencyService } from './../currency.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  rates: any;
 
+  constructor(private CurrencyService: CurrencyService) {}
+
+  ngOnInit(): void {
+    this.CurrencyService.getRates().subscribe((data) => {
+      this.rates = data.rates;
+    });
+  }
 }
